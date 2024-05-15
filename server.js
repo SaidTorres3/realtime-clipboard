a554c5406a3a6c7a7a39dd0bd211102bdaffb103
Said Torres <sesaid17@hotmail.com>
2024-05-15 17:17:50 -0600

    Changed port to 8088

--- server.js
+++ server.js
@@ -103,5 +103,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3000, '0.0.0.0', () => {
-  console.log('Server is running on http://0.0.0.0:3000');
+  console.log('Server is running on http://0.0.0.0:8088');
 });
